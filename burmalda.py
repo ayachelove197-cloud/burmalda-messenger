523a372d8833dde34a9650a86c3d52aa9ea27d2b
ayachelove197-cloud <ayachelove197@gmail.com>
2026-04-16 14:33:48 +0800

    burmalda.py

--- burmalda.py
+++ burmalda.py
@@ -1,4 +1,6 @@
 # ================= IMPORTS =================
+import eventlet
+eventlet.monkey_patch()
 import os
 import sqlite3
 import time
@@ -15,13 +17,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_burmalda_777'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-socketio = SocketIO(
-    app,
-    cors_allowed_origins="*",
-    async_mode="threading",
-    logger=False,
-    engineio_logger=False
-)
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
@@ -221,6 +217,4 @@ if __name__ == "__main__":
     socketio.run(
         app,
         host="0.0.0.0",
-        port=port,
-        allow_unsafe_werkzeug=True
-    )
+        port=port)
